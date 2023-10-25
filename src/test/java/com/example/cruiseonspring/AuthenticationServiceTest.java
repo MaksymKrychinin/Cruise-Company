72100c8e3664dcab41abd88d8576aa03e210bbd5
PlayMaks <maks1997clan@gmail.com>
2023-10-25 13:23:43 +0300

    TestChange

--- src/test/java/com/example/cruiseonspring/AuthenticationServiceTest.java
+++ src/test/java/com/example/cruiseonspring/AuthenticationServiceTest.java
@@ -6,16 +6,16 @@ import com.example.cruiseonspring.entity.Role;
 import com.example.cruiseonspring.entity.User;
 import com.example.cruiseonspring.mapper.UserMapper;
 import com.example.cruiseonspring.repository.UserRepository;
-import com.example.cruiseonspring.service.AuthenticationService;
 import com.example.cruiseonspring.service.AuthenticationServiceImpl;
 import com.example.cruiseonspring.service.JwtService;
 import org.junit.jupiter.api.*;
+import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.security.authentication.AuthenticationManager;
 
 import java.sql.Date;
+import java.util.Set;
 
 import static org.apache.commons.lang3.RandomStringUtils.randomNumeric;
 import static org.junit.jupiter.api.Assertions.assertTrue;
@@ -25,21 +25,26 @@ import static org.mockito.MockitoAnnotations.openMocks;
 @TestInstance(TestInstance.Lifecycle.PER_CLASS)
 @SpringBootTest
 public class AuthenticationServiceTest {
-    @Autowired
-    AuthenticationService authenticationService;
-    @Autowired
-    UserMapper userMapper;
+    @InjectMocks
+    final AuthenticationServiceImpl authenticationService;
+    final UserMapper userMapper;
     @Mock
     UserRepository userRepository;
-    @Autowired
-    JwtService jwtService;
-    @Autowired
-    AuthenticationManager authenticationManager;
+    final JwtService jwtService;
     AutoCloseable mockClose;
+
+    @Autowired
+    public AuthenticationServiceTest(AuthenticationServiceImpl authenticationService,
+                                     UserMapper userMapper,
+                                     JwtService jwtService) {
+        this.authenticationService = authenticationService;
+        this.userMapper = userMapper;
+        this.jwtService = jwtService;
+    }
 
     @BeforeAll
     public void setUp() {
-        openMocks(this);
+        mockClose = openMocks(this);
     }
 
     @AfterAll
@@ -57,7 +62,6 @@ public class AuthenticationServiceTest {
         AuthenticationResponse authenticationResponse = authenticationService.register(registerRequest);
         String token = authenticationResponse.getToken();
         boolean tokenValid = jwtService.isTokenValid(token, userFromRequest);
-        //RepositoryWillChangeObject assertEquals(userAfterMapper,userFromRequest);
         System.out.println(tokenValid);
         assertTrue(tokenValid);
     }
@@ -71,7 +75,7 @@ public class AuthenticationServiceTest {
                 .email(registerRequest.getEmail())
                 .password(registerRequest.getPassword())
                 .surname(registerRequest.getSurname())
-                .userRole(Role.builder().name("USER").build())
+                .userRoles(Set.of(Role.builder().name("USER").build()))
                 .id(1)
                 .build();
     }
