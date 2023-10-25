a262c8d5af36e497fdfb5a4123bc0eb84144d45e
PlayMaks <maks1997clan@gmail.com>
2023-10-25 12:43:40 +0300

    change request mapping to cruise-ships

--- src/main/java/com/example/cruiseonspring/controller/CruiseShipController.java
+++ src/main/java/com/example/cruiseonspring/controller/CruiseShipController.java
@@ -12,7 +12,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/api/v1/cruiseShips")
+@RequestMapping("/api/v1/cruise-ships")
 @RequiredArgsConstructor
 @CrossOrigin
 public class CruiseShipController {
