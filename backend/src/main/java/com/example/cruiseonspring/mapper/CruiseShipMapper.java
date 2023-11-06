f00ef5f0202fbf088d3fb407d0f25d83827a06b4
PlayMaks <maks1997clan@gmail.com>
2023-11-06 02:32:23 +0200

    edit Mappers/usage

--- backend/src/main/java/com/example/cruiseonspring/mapper/CruiseShipMapper.java
+++ backend/src/main/java/com/example/cruiseonspring/mapper/CruiseShipMapper.java
@@ -3,12 +3,9 @@ package com.example.cruiseonspring.mapper;
 import com.example.cruiseonspring.dto.CruiseShipDto;
 import com.example.cruiseonspring.entity.CruiseShip;
 import org.mapstruct.Mapper;
-import org.springframework.stereotype.Service;
-
-import java.util.function.Function;
 
 
-@Mapper
+@Mapper(componentModel = "spring")
 public interface CruiseShipMapper {
 
     CruiseShip cruiseShipToDto(CruiseShipDto source);
--- backend/src/main/java/com/example/cruiseonspring/mapper/UserMapper.java
+++ backend/src/main/java/com/example/cruiseonspring/mapper/UserMapper.java
@@ -1,19 +1,19 @@
 package com.example.cruiseonspring.mapper;
 
-import com.example.cruiseonspring.config.ApplicationConfig;
 import com.example.cruiseonspring.dto.RegisterRequest;
 import com.example.cruiseonspring.entity.Role;
 import com.example.cruiseonspring.entity.User;
 import com.example.cruiseonspring.repository.RoleRepository;
-import org.mapstruct.*;
-import org.mapstruct.control.MappingControl;
+import org.mapstruct.AfterMapping;
+import org.mapstruct.Mapper;
+import org.mapstruct.MappingTarget;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
 import java.util.Set;
 
 
-@Mapper(componentModel = "spring", uses = {RoleRepository.class, ApplicationConfig.class})
+@Mapper(componentModel = "spring")
 public abstract class UserMapper {
     @Autowired
     protected PasswordEncoder passwordEncoder;
@@ -22,9 +22,6 @@ public abstract class UserMapper {
 
 
     public abstract User registerRequestToUser(RegisterRequest registerRequest);
-
-    public abstract RegisterRequest userToRegisterRequest(User user);
-
     @AfterMapping
     protected void encodePassword(RegisterRequest registerRequest,@MappingTarget User.UserBuilder user) {
         user.password(passwordEncoder.encode(registerRequest.getPassword()));
