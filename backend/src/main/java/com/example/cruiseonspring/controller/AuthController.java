05fd890a571747a4a6e0a7940a0ed9b74064ddf5
PlayMaks <maks1997clan@gmail.com>
2023-11-06 22:06:08 +0200

    edit security

--- backend/src/main/java/com/example/cruiseonspring/controller/AuthController.java
+++ backend/src/main/java/com/example/cruiseonspring/controller/AuthController.java
@@ -3,7 +3,6 @@ package com.example.cruiseonspring.controller;
 import com.example.cruiseonspring.dto.AuthenticationRequest;
 import com.example.cruiseonspring.dto.AuthenticationResponse;
 import com.example.cruiseonspring.dto.RegisterRequest;
-import com.example.cruiseonspring.mapper.CruiseShipMapper;
 import com.example.cruiseonspring.service.AuthenticationService;
 import io.swagger.v3.oas.annotations.security.SecurityRequirements;
 import lombok.RequiredArgsConstructor;
