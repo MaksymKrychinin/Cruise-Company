58309ff2c6ec5f7773a1f6602298f4b5237adf85
maksym.k <maksym.k@protoq.dev>
2023-11-25 00:54:51 +0200

    feat(cs-qes): added jms handling

--- cruise-ship-executer-service/src/main/java/com/example/cruiseshipexecuterservice/service/JmsService.java
+++ cruise-ship-executer-service/src/main/java/com/example/cruiseshipexecuterservice/service/JmsService.java
@@ -1,6 +1,8 @@
 package com.example.cruiseshipexecuterservice.service;
 
 import com.example.cruiseshipexecuterservice.entity.jms.CruiseShip;
+import com.example.cruiseshipexecuterservice.entity.jms.User;
+import com.example.cruiseshipexecuterservice.entity.jms.UserOrder;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.jms.annotation.JmsListener;
@@ -12,10 +14,24 @@ import org.springframework.stereotype.Service;
 @Log4j2
 public class JmsService {
     private final StatisticService statisticService;
+    private final NotificationService notificationService;
 
     @JmsListener(destination = "cruise-ships")
     public void receiveAndForwardMessageFromQueue(
             @Payload CruiseShip cruiseShip) {
+        log.info(cruiseShip);
+        statisticService.updateStatistic(cruiseShip);
+        notificationService.sendNotification(cruiseShip);
+    }
+    @JmsListener(destination = "users")
+    public void receiveAndForwardMessageFromQueue(
+            @Payload User user) {
+        log.info(user);
+        statisticService.updateStatistic(user);
+    }
+    @JmsListener(destination = "user-orders")
+    public void receiveAndForwardMessageFromQueue(
+            @Payload UserOrder cruiseShip) {
         log.info(cruiseShip);
         statisticService.updateStatistic(cruiseShip);
     }
