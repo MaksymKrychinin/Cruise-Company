826557087a18e655b5bebd90f1daad563880a721
maksym.k <maksym.k@protoq.dev>
2023-12-17 02:02:07 +0200

    feat(cs-main-frontend): filter backend introduce

--- cs-cos-backend/src/main/java/com/example/cruiseonspring/repository/BaseSpecification.java
+++ cs-cos-backend/src/main/java/com/example/cruiseonspring/repository/BaseSpecification.java
@@ -1,23 +1,20 @@
 package com.example.cruiseonspring.repository;
 
-import jakarta.persistence.criteria.CriteriaBuilder;
-import jakarta.persistence.criteria.CriteriaQuery;
-import jakarta.persistence.criteria.Predicate;
-import jakarta.persistence.criteria.Root;
+import jakarta.persistence.criteria.*;
 import org.springframework.data.jpa.domain.Specification;
 
 public class BaseSpecification<T> implements Specification<T> {
 
     private String filterColumn;
     private String filterData;
-    private String joinTable;
+    private String[] joinTable;
 
     public BaseSpecification(String filterColumn, String filterData) {
         this.filterColumn = filterColumn;
         this.filterData = filterData;
     }
 
-    public BaseSpecification(String filterColumn, String filterData, String joinTable) {
+    public BaseSpecification(String filterColumn, String filterData, String... joinTable) {
         this.filterColumn = filterColumn;
         this.filterData = filterData;
         this.joinTable = joinTable;
@@ -27,7 +24,11 @@ public class BaseSpecification<T> implements Specification<T> {
     public Predicate toPredicate(Root<T> root, CriteriaQuery<?> query, CriteriaBuilder criteriaBuilder) {
         if (filterColumn != null) {
             if (joinTable != null) {
-                return criteriaBuilder.like(root.join(joinTable).get(filterColumn), "%" + filterData + "%");
+                Join<Object, Object> join = root.join(joinTable[0]);
+                for (int i = 1; i < joinTable.length; i++) {
+                    join = join.join(joinTable[i]);
+                }
+                return criteriaBuilder.like(join.get(filterColumn), "%" + filterData + "%");
             } else {
                 return criteriaBuilder.like(root.get(filterColumn), "%" + filterData + "%");
             }
