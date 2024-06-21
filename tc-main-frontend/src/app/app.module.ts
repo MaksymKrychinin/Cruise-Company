df241fee3a46bbb5efad46a273ac03b1e906170e
PlayMaks <maks1997clan@gmail.com>
2024-06-21 19:38:44 +0300

    feat/tc-main-frontend/add-lib-control

--- tc-main-frontend/src/app/app.module.ts
+++ tc-main-frontend/src/app/app.module.ts
@@ -2,7 +2,6 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import {TcDesignSystemModule} from "tc-design-system";
 
 @NgModule({
   declarations: [
@@ -10,7 +9,6 @@ import {TcDesignSystemModule} from "tc-design-system";
   ],
   imports: [
     BrowserModule,
-    TcDesignSystemModule
   ],
   providers: [],
   bootstrap: [AppComponent]
